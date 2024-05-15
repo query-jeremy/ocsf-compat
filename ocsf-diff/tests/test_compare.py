cce7c8d16a99f35828e593e9a7fe49884145027d
Jeremy Fisher <106115283+query-jeremy@users.noreply.github.com>
2024-05-15 12:10:47 -0500

    Adding tests

--- ocsf-diff/tests/test_compare.py
+++ ocsf-diff/tests/test_compare.py
@@ -1,29 +1,33 @@
 # pyright: reportPrivateUsage = false
-from typing import Optional
+from typing import Optional, Any
 
-from ocsf_diff.compare import compare
+from ocsf_diff.compare import compare, compare_dict
 from ocsf_diff.model import (
     Change,
     NoChange,
     DiffAttr,
+    DiffEnumMember,
     Removal,
     Addition,
 )
 from ocsf_schema import OcsfAttr, OcsfEnumMember
 
 
-def test_compare_primitive():
+def test_compare_primitives():
+    """Test compare() on primitive arguments (int, bool, list[int], Optional[_], etc.)."""
     assert compare(1, 2) == Change(before=1, after=2)
     assert compare(True, False) == Change(before=True, after=False)
     assert compare(True, True) == NoChange[bool]()
     assert compare([1, 2, 3], [1, 2]) == Change(before=[1, 2, 3], after=[1, 2])
+    assert compare("test1", "test2") == Change(before="test1", after="test2")
 
     x1: Optional[bool] = True
     x2: Optional[bool] = None
     assert compare(x1, x2) == Change(True, None)
 
 
-def test_compare_scalar():
+def test_compare_scalar_properties():
+    """Test compare() on scalar properties of an OcsfModel."""
     old_attr = OcsfAttr(caption="test1", description="", requirement="required", type="int_t")
     new_attr = OcsfAttr(caption="test2", description="", requirement="required", type="int_t")
     diff = compare(old_attr, new_attr)
@@ -32,10 +36,11 @@ def test_compare_scalar():
     assert diff.caption == Change(before="test1", after="test2")
     assert diff.description == NoChange()
     assert diff.requirement == NoChange()
-    assert diff.enum == {}
+    assert diff.enum == NoChange() #{}
 
 
-def test_compare_optional_change():
+def test_compare_optional_property():
+    """Test compare() on an optional property changing to and from None."""
     old_attr = OcsfAttr(caption="test1", group=None, description="", requirement="required", type="int_t")
     new_attr = OcsfAttr(caption="test2", group="test", description="", requirement="required", type="int_t")
     diff = compare(old_attr, new_attr)
@@ -43,8 +48,13 @@ def test_compare_optional_change():
     assert isinstance(diff, DiffAttr)
     assert diff.group == Change(before=None, after="test")
 
+    diff = compare(new_attr, old_attr)
 
-def test_optional_dict():
+    assert isinstance(diff, DiffAttr)
+    assert diff.group == Change(after=None, before="test")
+
+
+def test_compare_optional_dict_property():
     old_attr = OcsfAttr(
         caption="test",
         description="",
@@ -77,3 +87,67 @@ def test_optional_dict():
     assert diff.enum["-1"] == Removal(before=OcsfEnumMember(caption="Unknown"))
     assert diff.enum["1"] == NoChange()
     assert diff.enum["99"] == Addition(after=OcsfEnumMember(caption="Unknown"))
+
+def test_compare_dict_two_dicts():
+    d1: dict[int, str] = {1: "a", 2: "b", 3: "c"}
+    d2: dict[int, str] = {0: "a", 2: "b", 3: "d"}
+
+    diff = compare_dict(d1, d2)
+    assert isinstance(diff, dict)
+
+    for i in (0, 1, 2, 3):
+        assert i in diff
+
+    assert diff[0] == Addition(after="a")
+    assert diff[1] == Removal(before="a")
+    assert diff[2] == NoChange()
+    assert diff[3] == Change(before="c", after="d")
+
+def test_compare_dict_two_null():
+    diff: Any = compare_dict(None, None)
+    assert isinstance(diff, NoChange)
+    
+def test_compare_dict_one_dict():
+    d1: dict[int, str] = {1: "a", 2: "b", 3: "c"}
+
+    diff = compare_dict(d1, None)
+    assert isinstance(diff, dict)
+
+    for i in (1, 2, 3):
+        assert i in diff
+
+    assert diff[1] == Removal(before="a")
+    assert diff[2] == Removal(before="b")
+    assert diff[3] == Removal(before="c")
+
+    diff = compare_dict(None, d1)
+    assert isinstance(diff, dict)
+
+    for i in (1, 2, 3):
+        assert i in diff
+
+    assert diff[1] == Addition(after="a")
+    assert diff[2] == Addition(after="b")
+    assert diff[3] == Addition(after="c")
+
+def test_compare_dict_ocsf_models():
+    e1 = {
+        "1": OcsfEnumMember(caption="Other"),
+        "2": OcsfEnumMember(caption="Success"),
+        "3": OcsfEnumMember(caption="Failure"),
+        "4": OcsfEnumMember(caption="Unknown"),
+    }
+    e2 = {
+        "2": OcsfEnumMember(caption="Winning!"),
+        "3": OcsfEnumMember(caption="Failure"),
+        "4": OcsfEnumMember(caption="Unknown"),
+        "5": OcsfEnumMember(caption="Flattened"),
+    }
+
+    diff = compare_dict(e1, e2)
+    assert isinstance(diff, dict)
+    assert diff["1"] == Removal(before=OcsfEnumMember(caption="Other"))
+    assert diff["2"] == DiffEnumMember(caption=Change(before="Success", after="Winning!"))
+    assert diff["3"] == NoChange()
+    assert diff["4"] == NoChange()
+    assert diff["5"] == Addition(after=OcsfEnumMember(caption="Flattened"))
